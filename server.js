4cbc9059427e8ff0c06c5e89cba877d272859f6d
unknown <mack.mohanverma.mec19@itbhu.ac.in>
2022-05-16 22:21:00 +0530

    sdfsasasdg

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV === "production"){
     app.use(express.static("front-end/build"));
 }
 
-app.get('/',(req,res)=>{
+app.get('/app',(req,res)=>{
     res.send('I am on!!');
 });
 
