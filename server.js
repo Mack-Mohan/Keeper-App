5175c29d787cd1e7b45d7de90ec957b82ae64d1e
unknown <mack.mohanverma.mec19@itbhu.ac.in>
2022-05-17 00:26:39 +0530

    sxfkgjkgllgskjhcf

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require("mongoose");
+var mongoose = require("mongoose");
 const routes = require("./routes/notesApi");
 const dotenv = require("dotenv");
 const cors = require("cors");
@@ -20,11 +20,11 @@ mongoose.connect(process.env.DB_ACCESS || "mongodb://localhost:27017/notesDB", {
     useNewUrlParser: true
 });
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("front-end/build"));
-    app.get("*", function (req, res) {
-       res.sendFile(path.join(__dirname, "./front-end/build/index.html"));
-  });
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'front-end/build')));
+    app.get('*',(req,res)=>
+        res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html'))
+    );
   }
 else{
 app.get('/',(req,res)=>{
