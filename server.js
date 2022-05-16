bd042fa4402be8e498cbb926e6b1204895ad3168
unknown <mack.mohanverma.mec19@itbhu.ac.in>
2022-05-16 21:18:25 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/notesApi");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const app = express();
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+app.use('/app', routes);
+
+mongoose.connect(process.env.DB_ACCESS, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("front-end/build"));
+}
+
+app.get('/',(req,res)=>{
+    res.send('I am on!!');
+});
+
+app.listen(PORT);
